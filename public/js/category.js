2213e8ed77abca425ff827abf1166a3f570ce7ea
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-06 11:18:19 -0500

    added in the psuedo code for the necessary functions of the category js file

--- public/js/category.js
+++ public/js/category.js
@@ -2,5 +2,30 @@
 
 
 $(document).ready(function () {
+  // Getting references to the name input and category container and  table body
+
+  // Adding event listeners to the form to create a new object, and the button to delete
+
+  // Getting the initial list of categories
+
+  //function when the form is submitted to create a new category
+
+  // nothing happens if the field filled out
+
+  // call the function and pass the value of the name input
+
+  //function for creating a category. Calls getcategory once complete
+
+  //function for creating a new list row for categories 
+
+  //function for retting categories & then almost getting the to the page
+
+  //function for rendering the list of categories on the page
+
+  // alert function for what to render when there are no categories for a product
+
+  //function for what happens for the delete button
 
 });
+
+//end
