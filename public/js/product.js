99de062daef1d96513c74f01a4c8b101f5e514c9
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-06 14:05:31 -0500

    added the psuedo code/function needs for what need to be in the product js

--- public/js/product.js
+++ public/js/product.js
@@ -2,7 +2,45 @@
 
 
 $(document).ready(function () {
+    // product container that will hold the listings
 
+
+    // Click events for the edit / delete
+
+
+    // variable to hold the listings
+
+
+    // Need something for when there is a item listening for a specific category
+    // Looks for a query param in the url for category_id
+
+
+    // if there is no id, get all the products
+
+
+    // function to get all products from db and then updates
+
+
+    //[may//may not keep b/c we dont have authentication], 
+    //but function that does an API call to delete products/items
+
+
+    //function to append all the built post HTML inside products container
+
+
+    // function to construct the post html
+
+
+
+    // function to find product to be delete, then deletes
+
+
+    // function to fin which product to update, then takes to the appropriate url
+
+
+    // function to display a message when there are no
 
 
 });
+
+// end
