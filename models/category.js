50fbbac1ef0aff35a7bb16b8ea9df3bc0d83ce42
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-05 23:19:43 -0500

    started the Category Sequelize model

---
+++ models/category.js
@@ -0,0 +1,6 @@
+//Exporting the sequelize model for categories  
+
+module.exports = function (sequelize, DataTypes) {
+
+};
+
--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
--- Placeholder Schema file
 
 -- Drops the blogger if it exists currently --
--- DROP DATABASE IF EXISTS <Insert DB HERE>;
+DROP DATABASE IF EXISTS product_test;
 -- Creates the "blogger" database --
--- CREATE DATABASE <Insert DB HERE>;
+CREATE DATABASE product_test;
