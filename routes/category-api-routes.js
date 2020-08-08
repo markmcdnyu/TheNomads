1f282c5d4b1948591030ef445a54030ed36d3a7a
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-08 00:14:41 -0500

    added the post and delete routes for the category-route js file

--- routes/category-api-routes.js
+++ routes/category-api-routes.js
@@ -3,25 +3,45 @@
 var db = require("../models");
 
 module.exports = function (app) {
-
-
     // Get route for getting categories & the findAll
-
-    // Use include property, so [db.Product]
-
-
+    app.get("/api/categories", function (req, res) {
+        // Use include property, so [db.Product]
+        db.Category.findAll({
+            include: [db.Product]
+        }).then(function (dbCategory) {
+            res.json(dbCategory);
+        });
+    });
 
     // Get route for getting categories & the findOne
-
-    // Use include property, so [db.Product]
-
-
+    app.get("/api/categories/:id", function (req, res) {
+        // Use include property, so [db.Product]
+        db.Category.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Product]
+        }).then(function (dbCategory) {
+            res.json(dbCategory);
+        });
+    });
 
     // post route for categories 
-
+    app.post("/api/categories", function (req, res) {
+        db.Category.create(req.body).then(function (dbCategory) {
+            res.json(dbCategory);
+        });
+    });
 
     // delete route for categories 
-
-
+    app.delete("/api/categories/:id", function (req, res) {
+        db.Category.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbCategory) {
+            res.json(dbCategory);
+        });
+    });
 
 };
