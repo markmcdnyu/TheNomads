10fa159f506110af4a513c860b586947de6cc85e
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-08 00:09:54 -0500

    added the require db and module exports with the routes I will create - findAll, findOne, post and delete

--- routes/category-api-routes.js
+++ routes/category-api-routes.js
@@ -1,2 +1,27 @@
-// Placeholder for example code to this file below
+//Require db ..models
 
+var db = require("../models");
+
+module.exports = function (app) {
+
+
+    // Get route for getting categories & the findAll
+
+    // Use include property, so [db.Product]
+
+
+
+    // Get route for getting categories & the findOne
+
+    // Use include property, so [db.Product]
+
+
+
+    // post route for categories 
+
+
+    // delete route for categories 
+
+
+
+};
