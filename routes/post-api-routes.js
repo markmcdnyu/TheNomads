da350792859d00ae91d702fb5be64ab1e46d6c38
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-07 23:54:49 -0500

    Added the findall, find one, get and post routes

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -5,12 +5,41 @@ var db = require("../models");
 module.exports = function (app) {
 
     // Get route for getting all of the products
+    app.get("/api/products", function (req, res) {
+        var query = {};
+        if (req.query.category_id) {
+            query.CategoryId = req.query.category_id;
+        }
 
+        // Use include property, so [db.Category]
+        db.Product.findAll({
+            where: query,
+            include: [db.Category]
+        }).then(function (dbProduct) {
+            res.json(dbProduct);
+        });
+    });
 
     // Get route for retrieving a single product
+    app.get("/api/products/:id", function (req, res) {
 
+        // Use include property, so [db.Category]
+        db.Product.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Category]
+        }).then(function (dbProduct) {
+            res.json(dbProduct);
+        });
+    });
 
     // POST route for saving a new post
+    app.post("/api/products", function (req, res) {
+        db.Product.create(req.body).then(function (dbProduct) {
+            res.json(dbProduct);
+        });
+    });
 
 
     // DELETE route for deleting posts
