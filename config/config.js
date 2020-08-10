775ecb1c172a88ea07c22afe7eaa084dbf3c7244
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-10 00:49:32 -0500

    connection and db fixes

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     username: "root",
     password: "password",
-    database: "tradeup_db",
+    database: "tradeup_test",
     host: "127.0.0.1",
     port: 3306,
     dialect: "mysql"
