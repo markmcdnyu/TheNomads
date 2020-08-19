c5fec2a51a2f8b1a4d870720a26b5053fb92b3ab
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-18 21:11:18 -0500

    added the controllers folder w/ categories, product, and view js files

---
+++ controllers/category-controller.js
@@ -0,0 +1,7 @@
+// Requiring our models
+
+const db = require("../models");
+const express = require('express');
+const router = express.Router();
+
+// Find all categories and return them to the user 
---
+++ controllers/product-controller.js
@@ -0,0 +1,8 @@
+// Requiring our models
+const db = require("../models");
+const express = require('express');
+const router = express.Router();
+
+
+// Routes
+// =================================================
