8565f5e4d3e6a218b3df884317ce8adc932e5d6b
MannyMoses <msm.prof@hotmail.com>
2020-08-20 00:40:34 -0500

    modified cate-control

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -5,3 +5,43 @@ const express = require('express');
 const router = express.Router();
 
 // Find all categories and return them to the user 
+
+router.get("/api/categories", function (req, res) {
+    db.Category.findAll({
+        include: [db.Product]
+    }).then(function (dbCategory) {
+        res.json(dbCategory);
+    });
+});
+
+router.get("/api/categories/:id", function (req, res) {
+    db.Category.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.product]
+    }).then(function (dbCategory) {
+        res.json(dbCategory);
+    });
+});
+
+router.post("/api/categories", function (req, res) {
+    console.log(req.body)
+    db.Category.create(req.body).then(function (dbCategory) {
+        res.json(dbCategory);
+    });
+});
+
+router.delete("/api/categories/:id", function (req, res) {
+    db.Category.destroy({
+        where: {
+            id:req.params.id
+    }
+    }).then(function (dbCategory) {
+        res.json(dbCategory);
+    });
+});
+
+module.exports = router;
+
+//end
